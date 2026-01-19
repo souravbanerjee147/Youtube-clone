88a93490377abf521893f2f10c34af5bdebefdde
souravbanerjee147 <souravbanerjeeweb18@gmail.com>
2026-01-20 00:50:39 +0530

    constants.js uploaded

---
+++ frontend/src/utils/constants.js
@@ -0,0 +1,24 @@
+// API Base URL
+export const API_BASE_URL = 'http://localhost:5001/api';
+
+// Video Categories
+export const CATEGORIES = [
+  'All',
+  'Entertainment',
+  'Music',
+  'Gaming',
+  'Education',
+  'Technology',
+  'Sports',
+  'News',
+  'Other'
+];
+
+// Default video thumbnail
+export const DEFAULT_THUMBNAIL = 'https://images.unsplash.com/photo-1593359677879-a4bb92f829d1?w=320&h=180&fit=crop';
+
+// Default avatar
+export const DEFAULT_AVATAR = 'https://cdn-icons-png.flaticon.com/512/149/149071.png';
+
+// Default channel banner
+export const DEFAULT_BANNER = 'https://images.unsplash.com/photo-1517077304055-6e89abbf09b0?w=1920&h=250&fit=crop';
